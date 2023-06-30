90cab3918de760b26a3cc81e2b834a30971f22ac
dabin <duobin0626@gmail.com>
2023-06-30 22:27:36 +0900

    .padStart 주석설명 추가

--- javascript-practice/js/app2.js
+++ javascript-practice/js/app2.js
@@ -3,10 +3,12 @@ const 시작_시간 = new Date();
 function setTime() {
   const 현재_시간 = new Date();
   const 흐른_시간 = new Date(현재_시간 - 시작_시간);
-  const 분 = 흐른_시간.getMinutes().toString();
+  const 분 = 흐른_시간.getMinutes().toString(); // 1. 숫자이기 때문에 문자열로 변환
   const 초 = 흐른_시간.getSeconds().toString();
   const timeH1 = document.querySelector(".time");
   timeH1.innerText = `${분.padStart(2, "0")}:${초.padStart(2, "0")}`;
+  // 2.문자열로 바꿔준 변수를 .padStart을 이용하여 2자리 수로 표현해줌 .
+  // .padStart는 문자열에서만 지원 , 숫자는 오류발생
 }
 
 // ` 백틱 으로 감싸줘야 변수로 나타낼 수 있다.
